9372f31dc5d55be6c18ff22a19885d63eff0ba09
Piotrus00 <117126497+Piotrus00@users.noreply.github.com>
2025-01-17 01:39:08 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,40 +1,96 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <sys/wait.h>
+
+int sem_karnet_gotowy;
+
+void exe_a_proces(const char *path, const char *name);
+int utworz_nowy_semafor(int key);
+static void usun_semafor(int semafor, int sem_num);
+int utworz_kolejke(int key);
+void usun_kolejke(int msgid);
+
 int main() {
-    key_t shm_key = ftok("program", 65);
-    key_t sem_key = ftok("program", 66);
+    int sem1 = utworz_nowy_semafor(123);
+    int msgid = utworz_kolejke(555);
 
-    // Inicjalizacja pamięci współdzielonej
-    int shm_id = shmget(shm_key, sizeof(PamiecWspoldzielona), 0666 | IPC_CREAT);
-    if (shm_id == -1) {
-        perror("Blad tworzenia pamieci wspoldzielonej");
-        exit(1);
-    }
+    exe_a_proces("/home/kali/CLionProjects/untitled3/cmake-build-debug/Kasjer", "Kasjer");
+    exe_a_proces("/home/kali/CLionProjects/untitled3/cmake-build-debug/Kasjer", "Kasjer");
+    exe_a_proces("/home/kali/CLionProjects/untitled3/cmake-build-debug/Kasjer", "Kasjer");
+    exe_a_proces("/home/kali/CLionProjects/untitled3/cmake-build-debug/Kasjer", "Kasjer");
 
-    PamiecWspoldzielona *pamiec = (PamiecWspoldzielona *)shmat(shm_id, NULL, 0);
-    if (pamiec == (void *)-1) {
-        perror("Blad dolaczenia pamieci wspoldzielonej");
-        exit(1);
-    }
+    exe_a_proces("/home/kali/CLionProjects/untitled3/cmake-build-debug/Narciarz", "Narciarz");
 
-    pamiec->liczba_karnetow = 0;
 
-    // Inicjalizacja semaforów
-    int sem_id = semget(sem_key, 1, 0666 | IPC_CREAT);
-    if (sem_id == -1) {
-        perror("Blad tworzenia semaforow");
-        exit(1);
-    }
-
-    semctl(sem_id, 0, SETVAL, 1); // Ustawienie wartości początkowej semafora na 1
-
-    printf("[Main] Inicjalizacja zakończona. Startujemy!\n");
-
-    // Uruchomienie kasjera
-    Kasjer(pamiec, sem_id);
-
-    // Zwalnianie zasobów przy zakończeniu
-    shmdt(pamiec);
-    shmctl(shm_id, IPC_RMID, NULL);
-    semctl(sem_id, 0, IPC_RMID);
+    int status;
+    wait(&status);
+    wait(&status);
+    wait(&status);
+    wait(&status);
+    wait(&status);
+    usun_semafor(sem1, 0);
+    usun_kolejke(msgid);
 
     return 0;
+}
+
+
+void exe_a_proces(const char *path, const char *name) {
+    if (fork() == 0) {
+        execl(path, name, NULL);
+        perror("execl failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+int utworz_nowy_semafor(int key)
+{
+    int semafor;
+    semafor=semget(key,2,0777|IPC_CREAT);
+    if (semafor==-1)
+    {
+        printf("Nie moglem utworzyc nowego semafora.\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("Semafor zostal utworzony : %d\n",semafor);
+    }
+    return semafor;
+}
+
+static void usun_semafor(int semafor, int sem_num)
+{
+    int sem;
+    sem=semctl(semafor,sem_num,IPC_RMID);
+    if (sem==-1)
+    {
+        printf("Nie mozna usunac semafora.\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Semafor zostal usuniety : %d\n",sem);
+}
+
+void usun_kolejke(int msgid) {
+    if (msgctl(msgid, IPC_RMID, NULL) == -1) {
+        perror("Nie moglem usunac kolejki komunikatow");
+        exit(EXIT_FAILURE);
+    }
+    printf("Kolejka komunikatow (ID: %d) zostala usunieta.\n", msgid);
+}
+
+int utworz_kolejke(int key) {
+    int id = msgget(key, 0666 | IPC_CREAT);
+    if (id == -1) {
+        perror("Nie moglem utworzyc kolejki komunikatow");
+        exit(EXIT_FAILURE);
+    }
+    // printf("Utworzono kolejke komunikatow o ID: %d\n", id);
+    return id;
 }
